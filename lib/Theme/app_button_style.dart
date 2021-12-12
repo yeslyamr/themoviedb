db76df3008a3ad96841cc7bb3473d0342d57bead
Raiym <yeslyamr@gmail.com>
2021-12-12 14:26:34 +0600

    Initial Commit

---
+++ lib/Theme/app_button_style.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+abstract class AppButtonStyle {
+  static final linkButton = ButtonStyle(
+    padding: MaterialStateProperty.all(const EdgeInsets.all(0.0)),
+    visualDensity: const VisualDensity(horizontal: VisualDensity.minimumDensity, vertical: VisualDensity.minimumDensity),
+  );
+
+  static final footerLinkButtonStyle = ButtonStyle(
+      foregroundColor: MaterialStateProperty.all(Colors.white),
+      visualDensity: VisualDensity(horizontal: VisualDensity.minimumDensity, vertical: VisualDensity.minimumDensity)
+  );
+}
